1b8f76d5dcafba700e7a9e86cfcea288e19e7094
Karn12003 <nawakarnmek@gmail.com>
2021-06-05 00:21:08 +0700

    update

--- resources/js/Components/Common/ButtonPill.vue
+++ resources/js/Components/Common/ButtonPill.vue
@@ -1,6 +1,16 @@
 <template>
   <div
-    class="bg-purplen rounded-full py-0.5 px-3 text-white hover:bg-pinkk cursor-pointer shadow-lg"
+    class="
+      bg-purplen
+      rounded-full
+      py-0.5
+      px-3
+      text-white
+      hover:bg-pinkk
+      cursor-pointer
+      shadow-lg
+      text-center
+    "
   >
     <slot></slot>
   </div>
---
+++ resources/js/Pages/Course/viewAllCourses.vue
@@ -0,0 +1,119 @@
+<template>
+  <div>
+    <navbar />
+    <div class="pt-32">
+      <img src="/images/head.png" class="mx-auto" />
+      <div class="container mx-auto mt-10">
+        <div class="grid grid-cols-4 gap-y-14">
+          <div class="col-span-1 items-center">
+            <img
+              src="/images/default1.png"
+              alt=""
+              class="
+                mx-auto
+                rounded-course
+                cursor-pointer
+                shadow-2xl
+                hover:bg-red-200
+              "
+            />
+          </div>
+          <div class="col-span-1 items-center">
+            <img
+              src="/images/default1.png"
+              alt=""
+              class="mx-auto rounded-course cursor-pointer shadow-2xl"
+            />
+          </div>
+          <div class="col-span-1 items-center">
+            <img
+              src="/images/default1.png"
+              alt=""
+              class="mx-auto rounded-course cursor-pointer shadow-2xl"
+            />
+          </div>
+          <div class="col-span-1 items-center">
+            <img
+              src="/images/default1.png"
+              alt=""
+              class="mx-auto rounded-course cursor-pointer shadow-2xl"
+            />
+          </div>
+          <div class="col-span-1 items-center">
+            <img
+              src="/images/default1.png"
+              alt=""
+              class="mx-auto rounded-course cursor-pointer shadow-2xl"
+            />
+          </div>
+          <div class="col-span-1 items-center">
+            <img
+              src="/images/default1.png"
+              alt=""
+              class="mx-auto rounded-course cursor-pointer shadow-2xl"
+            />
+          </div>
+          <div class="col-span-1 items-center">
+            <img
+              src="/images/default1.png"
+              alt=""
+              class="mx-auto rounded-course cursor-pointer shadow-2xl"
+            />
+          </div>
+          <div class="col-span-1 items-center">
+            <img
+              src="/images/default1.png"
+              alt=""
+              class="mx-auto rounded-course cursor-pointer shadow-2xl"
+            />
+          </div>
+          <div class="col-span-1 items-center">
+            <img
+              src="/images/default1.png"
+              alt=""
+              class="mx-auto rounded-course cursor-pointer shadow-2xl"
+            />
+          </div>
+          <div class="col-span-1 items-center">
+            <img
+              src="/images/default1.png"
+              alt=""
+              class="mx-auto rounded-course cursor-pointer shadow-2xl"
+            />
+          </div>
+          <div class="col-span-1 items-center">
+            <img
+              src="/images/default1.png"
+              alt=""
+              class="mx-auto rounded-course cursor-pointer shadow-2xl"
+            />
+          </div>
+          <div class="col-span-1 items-center">
+            <img
+              src="/images/default1.png"
+              alt=""
+              class="mx-auto rounded-course cursor-pointer shadow-2xl"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br />
+  <br />
+  <br />
+  <br />
+  <img src="/images/bottom2.png" />
+  <Copyright />
+</template>
+<script>
+import Navbar from "@/Components/Common/Navbar.vue";
+import Copyright from "@/Components/Common/Copyright.vue";
+
+export default {
+  components: {
+    Navbar,
+    Copyright,
+  },
+};
+</script>
